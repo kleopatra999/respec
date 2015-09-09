457a4e2f36e8f16fe022edd760ce8edc3457df65
Marcos Caceres <marcos@marcosc.com>
2015-09-09 13:12:40 -0400

    Cleaned up include-config.js

--- js/core/include-config.js
+++ js/core/include-config.js
@@ -1,30 +1,34 @@
 // Module core/include-config
 // Inject's the document's configuration into the head as JSON.
+/*globals define*/
+'use strict';
 
 define(
   [],
-  function() {
+  function () {
+    function confFilter(key, val) {
+      // DefinitionMap contains array of DOM elements that aren't serializable
+      // we replace them by their id
+      if (key === 'definitionMap') {
+        var ret = {};
+        Object.keys(val).forEach(function (k) {
+          ret[k] = val[k].map(function (d) {
+            return d[0].id;
+          });
+        });
+        return ret;
+      }
+      return val;
+    }
     return {
-      run: function(conf, doc, cb, msg) {
-        msg.pub("start", "core/include-config");
-        var script = doc.createElement("script");
-        script.id = "respecFinalConfig";
-        var confFilter = function(key, val) {
-          // DefinitionMap contains array of DOM elements that aren't serializable
-          // we replace them by their id
-          if (key === "definitionMap") {
-            var ret = {};
-            Object.keys(val).forEach(function(k) {
-              ret[k] = val[k].map(function(d) { return d[0].id});
-            });
-            return ret;
-          }
-          return val;
-        }
+      run: function (conf, doc, cb, msg) {
+        msg.pub('start', 'core/include-config');
+        var script = doc.createElement('script');
+        script.id = 'respecFinalConfig';
         script.innerText = JSON.stringify(conf, confFilter, 2);
-        script.type = "application/json";
+        script.type = 'application/json';
         doc.head.appendChild(script);
-        msg.pub("end", "core/include-config");
+        msg.pub('end', 'core/include-config');
         cb();
       }
     };
